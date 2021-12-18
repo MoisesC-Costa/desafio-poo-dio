219be28774773af2cff5375afb53981d3396a49e
Moises Costa Caldas <sesiom.br@gmail.com>
2021-12-18 15:12:35 -0300

    asdasd

--- src/br/com/dio/desafio/blackjack/actions/Action.java
+++
@@ -1,8 +0,0 @@
-package br.com.dio.desafio.blackjack.actions;
-
-import br.com.dio.desafio.blackjack.deck.Deck;
-import br.com.dio.desafio.blackjack.player.Player;
-
-public interface Action {
-    public void peform(Player author);
-}
